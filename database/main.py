801db26ba7eae4a2722e45f62316ffb174a62fce
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-08-28 10:14:45 +0300

    add: test for mongo connection

---
+++ database/main.py
@@ -0,0 +1,23 @@
+"""
+Hand using Mongo
+"""
+
+from .engine import MongoDBEngine
+
+
+def print_all_data():
+    """For visualisate"""
+    mongo = MongoDBEngine()
+    db_client = mongo.create_client()
+    db = db_client[mongo.get_db_name()]
+    collections = db.list_collection_names()
+
+    for collection_name in collections:
+        collection = db[collection_name]
+        print(f"\nDocuments in collection '{collection_name}':")
+        for document in collection.find():
+            print(document)
+
+
+if __name__ == "__main__":
+    print_all_data()
---
+++ database/tests.py
@@ -0,0 +1,25 @@
+"""
+Tests for database
+"""
+
+import unittest
+from engine import MongoDBEngine
+
+
+class TestMongo(unittest.TestCase):
+    """
+    Testing MongoDB works
+    """
+
+    def test_check_connection(self) -> bool:
+        """
+        Checking connection to MongoDB
+        """
+        for _ in range(5):
+            client = MongoDBEngine().create_client()
+            client.admin.command("ping")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
